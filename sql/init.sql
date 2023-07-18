0822ed82638b11915ddf818ee279a1349ab3a94d
Chelsea486MHz <mail@chelsea486mhz.fr>
2023-07-18 20:44:54 +0200

    Changed MariaDB initalization

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,8 @@
-CREATE TABLE token (
+USE readthat;
+
+CREATE TABLE IF NOT EXIST token (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  token VARCHAR(255) NOT NULL
+  token VARCHAR(48) NOT NULL
 );
 
 INSERT INTO token (token) VALUES ('replace-this-token');
