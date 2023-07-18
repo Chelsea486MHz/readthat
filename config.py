8de230894b02f5e390c56a3650064612a6d0efca
Chelsea486MHz <mail@chelsea486mhz.fr>
2023-07-18 20:05:12 +0200

    Added support for environment variables

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 TTS_LANGUAGE = 'en'
 
-DATABASE_NAME = 'db_name'
-DATABASE_HOST = 'db_host'
-DATABASE_USER = 'db_username'
-DATABASE_PASS = 'db_password'
+DATABASE_NAME = os.environ.get('DATABASE_NAME', 'db_name')
+DATABASE_HOST = os.environ.get('DATABASE_HOST', 'db_host')
+DATABASE_USER = os.environ.get('DATABASE_USER', 'db_username')
+DATABASE_PASS = os.environ.get('DATABASE_PASS', 'db_password')
 
 DATABASE_URI = 'mysql+pymysql://' + DATABASE_USER + ':' + DATABASE_PASS + '@' + DATABASE_HOST + '/' + DATABASE_NAME
